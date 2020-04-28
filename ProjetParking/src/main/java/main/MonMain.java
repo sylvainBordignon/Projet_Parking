e6a1d5f0f3a15701a7beb1397e1f3f6b7b2dec31
sylvainbordignon <sylvainbordignon@macbook-pro-de-sylvain.home>
2020-04-28 12:23:57 +0200

    Ajout du fichier de config pour communiquer avec mysql. Ajout de la classe main pour exécuter le programme.

---
+++ ProjetParking/src/main/java/main/MonMain.java
@@ -0,0 +1,16 @@
+
+package main;
+
+import java.sql.Connection;
+
+import ConnexionBDD.Connexion;
+
+public class MonMain {
+
+	public static void main(String[] args) {
+		
+		  Connection maConnexion = Connexion.getInstance();
+	
+	}
+
+}
