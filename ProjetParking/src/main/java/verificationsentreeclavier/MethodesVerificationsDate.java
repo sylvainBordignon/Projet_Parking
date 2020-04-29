1825cf779baebb8a3c51120ac20e64e3cc013c5f
sylvainbordignon <sylvainbordignon@macbook-pro-de-sylvain.home>
2020-04-29 14:45:58 +0200

    Ajout des vérifs entrée clavier pour la date.

---
+++ ProjetParking/src/main/java/verificationsentreeclavier/MethodesVerificationsDate.java
@@ -0,0 +1,40 @@
+package verificationsentreeclavier;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class MethodesVerificationsDate {
+
+	
+	  public boolean estValideDate(String dateUtilisateur) {
+		  
+	        DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	        sdf.setLenient(false);
+	        try {
+	            sdf.parse(dateUtilisateur);
+	        } catch (ParseException e) {
+	            return false;
+	        }
+	        System.out.println("OK");
+	        return true;
+	    }
+	  
+	  public boolean estValideHeureMinute(String dateUtilisateur) {
+		  
+	        DateFormat sdf = new SimpleDateFormat("HH:mm");
+	        sdf.setLenient(false);
+	        try {
+	            sdf.parse(dateUtilisateur);
+	        } catch (ParseException e) {
+	            return false;
+	        }
+	        System.out.println("OK");
+	        return true;
+	    }
+	  
+	  
+	}	
+	
+	
+
