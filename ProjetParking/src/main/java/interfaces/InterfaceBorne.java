4fba04d8ac03d6192186e09a83a3017bfb2bde3d
Hiba <hiba.safir@gmail.com>
2020-04-30 15:48:50 +0200

    Corrections dans l'interface de la borne.

--- ProjetParking/src/main/java/interfaces/InterfaceBorne.java
+++ ProjetParking/src/main/java/interfaces/InterfaceBorne.java
@@ -1,48 +1,49 @@
 package interfaces;
+
 import java.util.Scanner;
 
 public class InterfaceBorne {
-	public static final String messageErreur="Veuillez entrer une information correcte.";
-	public static final String messageAcceptation="Vous occupez la place ... pour une durée de ... Merci de votre visite.";
-	public static final String messageDedommagement="Il n'y a pas de places disponibles. Vous recevrez un dédommagement. Veuillez nous excuser.";
-	public static final String messageSansDedommagement="Il n'y a pas de places disponibles. Veuillez nous excuser.";
-	public static int placesLibres = 0;
+	public static final String MESSAGE_ERREUR="Veuillez entrer une information correcte.";
+	public static final String MESSAGE_ACCEPTATION="Vous occupez la place ... pour une durée de ... Merci de votre visite.";
+	public static final String MESSAGE_DEDOMMAGEMENT="Il n'y a pas de places disponibles. Vous recevrez un dédommagement. Veuillez nous excuser.";
+	public static final String MESSAGE_SANS_DEDOMMAGEMENT="Il n'y a pas de places disponibles. Veuillez nous excuser.";
+	public static final int PLACES_LIBRES = 3;
+	public static final String RETOUR_MENU_PRINCIPAL = "\nTapez '-1' pour retourner au menu principal";
 	
 	public static void lancerInterfaceBorne() {
-		System.out.println("Avez vous une réservation?" + "\n" + "1 - Oui" + "\n" + "2 - Non");
-		Scanner in = new Scanner(System.in);
-		String reponse = in.nextLine();
-		while(!reponse.equals("1") && !reponse.equals("2")) { 
-			System.out.println(messageErreur);
-			in = new Scanner(System.in);
-			reponse = in.nextLine();
-		}
+		System.out.println("Bienvenue sur l'interface du parking. \nAvez vous une réservation?" + "\n" + "1 - Oui" + "\n" + "2 - Non" + RETOUR_MENU_PRINCIPAL);
+		String reponse = InterfaceBorne.reessayer();
 		if (reponse.equals("1")) {
-			if(placesLibres==0)
-				System.out.println(messageSansDedommagement);
+			if(PLACES_LIBRES==0)
+				System.out.println(MESSAGE_DEDOMMAGEMENT);
 			else
 				InterfaceBorne.menuReservation();
 		}
 		else if (reponse.equals("2")) {
 			InterfaceBorne.menuSansReservation();
 		}
+		else if (reponse.equals("-1")) {//retour menu principal
+			InterfaceGenerale.interfaceGenerale();
+		}
 	}//v�rifier qu'il y a des places
 
 	private static void menuReservation() {
-		System.out.println("Veuillez entrer votre numéro de réservation.");
+		System.out.println("Veuillez entrer votre numéro de réservation." + RETOUR_MENU_PRINCIPAL);
 		Scanner in = new Scanner(System.in);
 		String numReservation = in.nextLine();
 		while(numReservation.equalsIgnoreCase("incorrect")) {//on v�rifie que le format est correct
-			System.out.println(messageErreur);
+			System.out.println(MESSAGE_ERREUR);
 			in = new Scanner(System.in);
 			numReservation = in.nextLine();
 		}
 		if (numReservation.equalsIgnoreCase("existe")) {//v�rification que le num�ro reservation existe dans la base
-			if(placesLibres==0) 
-				System.out.println(messageDedommagement);
+			if(PLACES_LIBRES==0) 
+				System.out.println(MESSAGE_DEDOMMAGEMENT);
 			else
-				System.out.println(messageAcceptation);//on enregistre la pr�sence du client dans la base
+				System.out.println(MESSAGE_ACCEPTATION);//on enregistre la pr�sence du client dans la base
 		}
+		else if (numReservation.equals("-1"))//retour menu principal
+			InterfaceGenerale.interfaceGenerale();
 		else {//le num�ro n'existe pas dans la base
 			System.out.println("Numéro de reservation inconnu. Voulez vous reessayer?" + "\n" + "1 - Oui" + "\n" + "2 - Non");
 			String reponse = InterfaceBorne.reessayer();
@@ -55,89 +56,95 @@ public class InterfaceBorne {
 	}
 	
 	private static void menuSansReservation() {
-		System.out.println("Veuillez entrer votre plaque d'immatriculation.");
+		System.out.println("Veuillez entrer votre plaque d'immatriculation." + RETOUR_MENU_PRINCIPAL);
 		Scanner in = new Scanner(System.in);
 		String immatriculation = in.nextLine();
 		while(immatriculation.equalsIgnoreCase("incorrect")) {//on v�rifie que le format est correct
-			System.out.println(messageErreur);
+			System.out.println(MESSAGE_ERREUR);
 			in = new Scanner(System.in);
 			immatriculation = in.nextLine();
 		}
-		if (!immatriculation.equalsIgnoreCase("incorrect")) {//v�rification que l'immatriculation existe dans la base
-			if(immatriculation.equalsIgnoreCase("existe")) {//v�rification qu'une reservation existe � cette date
-				if(placesLibres==0)
-					System.out.println(messageDedommagement);
-				else
-					System.out.println(messageAcceptation);//on enregistre la pr�sence du client dans la base
-			}
-			else {
-				if(immatriculation.equalsIgnoreCase("existePas")) {
-					System.out.println("Vous n'avez pas de réservation à cette date.");
-					if(placesLibres==0)
-						System.out.println(messageSansDedommagement);
-					else
-						InterfaceBorne.dureeSejour();
-				}
-				else {
-				System.out.println("Numéro d'immatriculation inconnu. Voulez vous reessayer?" + "\n" + "1 - Oui" + "\n" + "2 - Non");
-				String reponse = InterfaceBorne.reessayer();
-				if (reponse.equals("1"))
-					InterfaceBorne.menuSansReservation();
-				else if (reponse.equals("2"))
-					InterfaceBorne.verifNumClient();
-				}
-			}
+		if (immatriculation.equalsIgnoreCase("plaqueInconnue")) {//la plaque n'existe pas dans la base
+			System.out.println("Numéro d'immatriculation inconnu. Voulez vous reessayer?" + "\n" + "1 - Oui" + "\n" + "2 - Non");
+			String reponse = InterfaceBorne.reessayer();
+			if (reponse.equals("1"))
+				InterfaceBorne.menuSansReservation();
+			else if (reponse.equals("2"))
+				InterfaceBorne.verifNumClient();
+		}
+		else {//la plaque existe, on v�rifie qu'une reservation existe
+			InterfaceBorne.verifReservationExiste(immatriculation);
 		}
 	}
 	
 	private static void verifNumClient() {
-		System.out.println("Veuillez entrer votre numéro de client.");
+		System.out.println("Veuillez entrer votre numéro de client." + RETOUR_MENU_PRINCIPAL);
 		Scanner in = new Scanner(System.in);
 		String numClient = in.nextLine();
-		while(!numClient.equals("1") && !numClient.equals("2")) {//on v�rifie que le format est correct
-			System.out.println(messageErreur);
+		while(numClient.equalsIgnoreCase("incorrect")) {//on v�rifie que le format est correct
+			System.out.println(MESSAGE_ERREUR);
 			in = new Scanner(System.in);
 			numClient = in.nextLine();
 		}
-		if (numClient.equals("1")) {//v�rification que le num�ro client existe dans la base
-			InterfaceBorne.dureeSejour();
-		}
-		else if (numClient.equals("2")){//le num�ro n'existe pas dans la base
+		if (numClient.equalsIgnoreCase("numeroIconnu")){//le num�ro n'existe pas dans la base
 			System.out.println("Numéro de client inconnu. Voulez vous reessayer?" + "\n" + "1 - Oui" + "\n" + "2 - Non");
 			String reponse = InterfaceBorne.reessayer();
 			if (reponse.equals("1"))
 				InterfaceBorne.verifNumClient();
-			else if (reponse.equals("2"))
+			else if (reponse.equals("2"))//numéro de client non reconnu
 				System.out.println("Veuillez vous diriger vers la sortie. Merci pour votre visite.");
-		}		
+		}
+		else {//le numero existe dans la base, on vérifie qu'une reservation existe
+			InterfaceBorne.verifReservationExiste(numClient);
+		}
+	}
+	
+	private static void verifReservationExiste(String immatriculationOuClient) {
+		if(immatriculationOuClient.equalsIgnoreCase("existe")) {//v�rification qu'une reservation existe � cette date
+			if(PLACES_LIBRES==0)//s'il n'y a plus de place
+				System.out.println(MESSAGE_DEDOMMAGEMENT);
+			else
+				System.out.println(MESSAGE_ACCEPTATION);//on enregistre la pr�sence du client dans la base
+		}
+		else if(immatriculationOuClient.equalsIgnoreCase("existePas")) {
+				System.out.println("Vous n'avez pas de réservation à cette date.");
+				if(PLACES_LIBRES==0)
+					System.out.println(MESSAGE_SANS_DEDOMMAGEMENT);
+				else
+					InterfaceBorne.dureeSejour();
+			}
+		else if (immatriculationOuClient.equals("-1"))//retour au menu principal
+			InterfaceGenerale.interfaceGenerale();
 	}
 	
 	private static void dureeSejour() {
-		System.out.println("Combien de temps souhaitez vous rester? (en minutes)");
+		System.out.println("Combien de temps souhaitez vous rester? (en minutes)" + RETOUR_MENU_PRINCIPAL);
 		Scanner in = new Scanner(System.in);
-		int duree = in.nextInt();
-		while(duree<0 || duree>100) {//on v�rifie que le format est correct
-			System.out.println(messageErreur);
+		String duree = in.nextLine();
+		while(duree.equals("incorrect")) {//on v�rifie que le format est correct
+			System.out.println(MESSAGE_ERREUR);
 			in = new Scanner(System.in);
-			duree = in.nextInt();
+			duree = in.nextLine();
 		}
-		//si la duree est correcte
-		System.out.println(messageAcceptation);//on enregistre la pr�sence du client dans la base
-	}
-	
-	public static void main(String[] args) {
-		  InterfaceBorne.lancerInterfaceBorne();
+		if(duree.equals("-1"))//retour au menu principal
+			InterfaceGenerale.interfaceGenerale();
+		else//si la duree est correcte
+			System.out.println(MESSAGE_ACCEPTATION);//on enregistre la pr�sence du client dans la base
 	}
 	
 	public static String reessayer() {
 		Scanner in = new Scanner(System.in);
 		String reponse = in.nextLine();
-		while(!reponse.equals("1") && !reponse.equals("2")) { 
-			System.out.println(messageErreur);
+		while(!reponse.equals("1") && !reponse.equals("2") && !reponse.equals("-1")) { 
+			System.out.println(MESSAGE_ERREUR);
 			in = new Scanner(System.in);
 			reponse = in.nextLine();
 		}
 		return reponse;
+	}
+	
+	public static void main(String[] args) {
+		  InterfaceBorne.lancerInterfaceBorne();
 	}
 
 }
